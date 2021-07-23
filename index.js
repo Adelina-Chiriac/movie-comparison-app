56d7b7ce629bf42b7394d124cb9c35c167d13552
adelina2222 <adelinachiriac@gmail.com>
2021-07-23 13:10:38 +0100

    Test the API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello!');
+const fetchData = async () => {
+    const response = await axios.get("https://omdbapi.com/", {
+        params: {
+            apikey: "eeeb0f72",
+            s: "The Shining"
+        }
+    });
+    console.log(response.data);
+};
+
+fetchData();
